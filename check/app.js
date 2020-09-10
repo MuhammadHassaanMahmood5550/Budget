019ad6549480bf0c8ea5643886fac82a85976f3f
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-09-11 00:58:39 +0500

    minor change in code

--- check/app.js
+++ check/app.js
@@ -2,90 +2,95 @@
 
 // //Not working well
 
-// //Budget CONTROLLER
-// var budgeController =( function(){
+//Budget CONTROLLER
+ var budgeController =( function(){
 
-// })();
+ })();
 
-// // UI CONTROLLER
-// var UIController = (function () {
+// UI CONTROLLER
+ var UIController = (function () {
 
-//   var AccessClasses = {
-//   aType: ".add__type",
-//   aText: ".add__description",
-//   aNumber: ".add__value",
-//   addBtn: ".add__btn",
-// };
+   var AccessClasses = {
+      aType: ".add__type",
+      aText: ".add__description",
+      aNumber: ".add__value",
+      addBtn: ".add__btn",
+ };
+ 
 
-//   var AccessInput = {
-//     type: document.querySelector(AccessClasses.aType).value,
-//      text: document.querySelector(AccessClasses.aText).value,
-//      number: document.querySelector(AccessClasses.aNumber).value,
-//  };
-
-//   return {  
-//   //Exposed AccessClasses method
-//   getClasses: function(){
-//      return AccessClasses;
-//   },
-//  //Exposed Accessinput method
-//   getInputs: function(){
-//       return AccessInput;
-//    }
-
-// };
-
-// })();
+  
+  /* if(AccessInput.text == ""){
+    return document.getElementById('add_description').value
+  } */
 
 
-// // APP GLOBLE CONTROLLER
-// var controller = (function (budgeCont, UICont) {
+   return {  
+   //Exposed AccessClasses method
+   getClasses: function(){
+      return AccessClasses;
+   },
+  //Exposed Accessinput method
+/*    getInputs: function(){
+       return AccessInput;
+    }
+    */
+ };
 
-// //SETUP EVENT-LISTENERS IN ONE FUN
-// var setupEventListeners = function(){
-//   var UIClasses = UICont.getClasses();  
-// document.querySelector(UIClasses.addBtn).addEventListener("click", addItems);
+ })();
 
-// document.addEventListener("keypress", function (event) {
-//   if (event.key === "Enter") {
-//     addItems();
-//   }
-// });
 
-// };
+// APP GLOBLE CONTROLLER
+ var controller = (function (budgeCont, UICont) {
+
+//SETUP EVENT-LISTENERS IN ONE FUN
+ var setupEventListeners = function(){
+ var UIClasses = UICont.getClasses();  
+ document.querySelector(UIClasses.addBtn).addEventListener("click", addItems);
+
+ document.addEventListener("keypress", function (event) {
+   if (event.key === "Enter") {
+     addItems();
+   }
+ });
+
+ };
 
 // //ADD DATA
-// var addItems = function () {
-//   //VARIABLES
-//   var input;
+ var addItems = function () {
+   //VARIABLES
+   var input;
   
-//   //TAKE USER TYPED INPUT  
-//   input = UICont.getInputs();
-//   //To check what user input
-//   console.log(input);   
+      var AccessInput = {
+       type: document.getElementById('add_type').value,
+       text: document.getElementById('add_description').value,
+       number: document.getElementById('add_value').value,
+  };
 
-// };
+  
+   //TAKE USER TYPED INPUT  
+   input = AccessInput
+   //To check what user input
+   console.log('something',input);   
+
+ };
 
 // // INIT FUNCTION
-// return{
-//   init: function(){
-//     setupEventListeners();
-//     console.log("started");
-//   }
-// };
+ return{
+   init: function(){
+     setupEventListeners();
+     console.log("started");
+   }
+ };
 
-// })(budgeController, UIController);
+ })(budgeController, UIController);
 
-// controller.init();
-
-
-
+controller.init();
 
 
 //WORKING WELL
 
 //Budget CONTROLLER
-var budgeController =( function(){
+/* ar budgeController =( function(){
 
 })();
 
@@ -162,4 +167,4 @@ return{
 
 })(budgeController, UIController);
 
-controller.init();
+controller.init(); */
